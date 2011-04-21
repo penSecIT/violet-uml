23c3ae1adf4a17e104b185c835853bb03f54dc5e
alexdp <alexdp@a3dc6853-a221-45bc-9d17-2c9bfbe3703c>
2011-04-21 05:11:23 +0000

    bean injection now supports inherited attributes
    git-svn-id: https://violet.svn.sourceforge.net/svnroot/violet@118 a3dc6853-a221-45bc-9d17-2c9bfbe3703c

--- VioletFramework/src/main/java/com/horstmann/violet/framework/injection/bean/SpringDependencyInjector.java
+++ VioletFramework/src/main/java/com/horstmann/violet/framework/injection/bean/SpringDependencyInjector.java
@@ -1,6 +1,8 @@
 package com.horstmann.violet.framework.injection.bean;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
@@ -49,34 +51,57 @@ public class SpringDependencyInjector implements ApplicationContextAware
      * @param o
      */
     public void inject(Object o) {
-        // Injects on fields
-        for (Field aField : o.getClass().getDeclaredFields())
-        {
-            SpringBean propertyAnnotation = aField.getAnnotation(SpringBean.class);
-            if (propertyAnnotation != null) {
-                String beanName = propertyAnnotation.name();
-                if ("".equals(beanName)) {
-                    beanName = aField.getName();
-                }
-                Object beanToInject = this.context.getBean(beanName);
-                if (beanToInject == null) {
-                    continue;
-                }
-                aField.setAccessible(true);
-                try
-                {
-                    aField.set(o, beanToInject);
-                }
-                catch (IllegalArgumentException e)
-                {
-                    throw new RuntimeException("Error while injecting bean maanged by Spring", e);
-                }
-                catch (IllegalAccessException e)
-                {
-                    throw new RuntimeException("Error while injecting bean managed by Spring", e);
+        List<Class<?>> classAndSuperClasses = getClassAndSuperClasses(o);
+        for (Class<?> aClass : classAndSuperClasses) {
+            // Injects on fields
+            for (Field aField : aClass.getDeclaredFields())
+            {
+                SpringBean propertyAnnotation = aField.getAnnotation(SpringBean.class);
+                if (propertyAnnotation != null) {
+                    String beanName = propertyAnnotation.name();
+                    if ("".equals(beanName)) {
+                        beanName = aField.getName();
+                    }
+                    Object beanToInject = this.context.getBean(beanName);
+                    if (beanToInject == null) {
+                        continue;
+                    }
+                    aField.setAccessible(true);
+                    try
+                    {
+                        aField.set(o, beanToInject);
+                    }
+                    catch (IllegalArgumentException e)
+                    {
+                        throw new RuntimeException("Error while injecting bean maanged by Spring", e);
+                    }
+                    catch (IllegalAccessException e)
+                    {
+                        throw new RuntimeException("Error while injecting bean managed by Spring", e);
+                    }
                 }
             }
         }
+    }
+    
+    /**
+     * Takes an objet and returns its class and all its inherited classes 
+     * @param o
+     * @return
+     */
+    private List<Class<?>> getClassAndSuperClasses(Object o) {
+        List<Class<?>> result = new ArrayList<Class<?>>();
+        List<Class<?>> fifo = new ArrayList<Class<?>>();
+        fifo.add(o.getClass());
+        while (!fifo.isEmpty()) {
+            Class<?> aClass = fifo.remove(0);;
+            Class<?> aSuperClass = aClass.getSuperclass();
+            if (aSuperClass != null) {
+                fifo.add(aSuperClass);
+            }
+            result.add(aClass);
+        }
+        return result;
     }
     
     /**
