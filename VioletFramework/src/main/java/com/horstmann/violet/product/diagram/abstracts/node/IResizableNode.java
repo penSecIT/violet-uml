c460c4960568b3ccda70fe47628f5504e6b677a1
alexdp <alexdp@a3dc6853-a221-45bc-9d17-2c9bfbe3703c>
2011-10-10 21:54:10 +0000

    refactoring
    git-svn-id: https://violet.svn.sourceforge.net/svnroot/violet@173 a3dc6853-a221-45bc-9d17-2c9bfbe3703c

--- VioletFramework/src/main/java/com/horstmann/violet/product/diagram/abstracts/node/IResizableNode.java
+++
@@ -1,8 +0,0 @@
-package com.horstmann.violet.product.diagram.abstracts.node;
-
-import java.awt.geom.Rectangle2D;
-
-public interface IResizableNode
-{
-    public void setWantedSize(Rectangle2D size);
-}
