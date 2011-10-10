ce3879f8d9ae28d2ad49807688d61571e99fc74b
alexdp <alexdp@a3dc6853-a221-45bc-9d17-2c9bfbe3703c>
2011-10-10 22:02:47 +0000

    refactoring
    git-svn-id: https://violet.svn.sourceforge.net/svnroot/violet@174 a3dc6853-a221-45bc-9d17-2c9bfbe3703c

---
+++ VioletFramework/src/main/java/com/horstmann/violet/workspace/statusbar/IStatusBar.java
@@ -0,0 +1,13 @@
+package com.horstmann.violet.workspace.statusbar;
+
+import java.awt.Component;
+
+public interface IStatusBar
+{
+
+    /**
+     * @return graphical component
+     */
+    public abstract Component getAWTComponent();
+
+}
