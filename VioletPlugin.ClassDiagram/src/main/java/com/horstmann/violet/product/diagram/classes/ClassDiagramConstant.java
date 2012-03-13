3cad4e6dbc42978256ff66652d1dda3b45c88eee
alexdp <alexdp@a3dc6853-a221-45bc-9d17-2c9bfbe3703c>
2012-03-13 00:43:24 +0000

    problem with svn
    git-svn-id: https://violet.svn.sourceforge.net/svnroot/violet@237 a3dc6853-a221-45bc-9d17-2c9bfbe3703c

--- VioletPlugin.ClassDiagram/src/main/java/com/horstmann/violet/product/diagram/classes/ClassDiagramConstant.java
+++
@@ -1,8 +0,0 @@
-package com.horstmann.violet.product.diagram.classes;
-
-public interface ClassDiagramConstant
-{
-
-    public static final String CLASS_DIAGRAM_STRINGS = "properties.ClassDiagramGraphStrings";
-    
-}
