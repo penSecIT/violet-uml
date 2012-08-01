b13d23b0db1d2efe0c3cf3b144cf805c50214651
alexdp <alexdp@a3dc6853-a221-45bc-9d17-2c9bfbe3703c>
2012-08-01 08:30:34 +0000

    + small refactoring
    git-svn-id: https://violet.svn.sourceforge.net/svnroot/violet@255 a3dc6853-a221-45bc-9d17-2c9bfbe3703c

--- VioletProduct.swing/src/main/java/com/horstmann/violet/UMLEditorApplication.java
+++ VioletProduct.swing/src/main/java/com/horstmann/violet/UMLEditorApplication.java
@@ -48,6 +48,7 @@ import com.horstmann.violet.framework.injection.bean.ManiocFramework.BeanInjecto
 import com.horstmann.violet.framework.injection.bean.ManiocFramework.InjectedBean;
 import com.horstmann.violet.framework.plugin.PluginLoader;
 import com.horstmann.violet.framework.theme.ClassicMetalTheme;
+import com.horstmann.violet.framework.theme.DarkAmbianceTheme;
 import com.horstmann.violet.framework.theme.ITheme;
 import com.horstmann.violet.framework.theme.ThemeManager;
 import com.horstmann.violet.framework.theme.VistaBlueTheme;
@@ -116,9 +117,11 @@ public class UMLEditorApplication
         ThemeManager themeManager = new ThemeManager();
         ITheme theme1 = new ClassicMetalTheme();
         ITheme theme2 = new VistaBlueTheme();
+        ITheme theme3 = new DarkAmbianceTheme();
         List<ITheme> themeList = new ArrayList<ITheme>();
         themeList.add(theme1);
         themeList.add(theme2);
+        themeList.add(theme3);
         themeManager.setInstalledThemes(themeList);
         themeManager.applyPreferedTheme();
         BeanFactory.getFactory().register(ThemeManager.class, themeManager);
