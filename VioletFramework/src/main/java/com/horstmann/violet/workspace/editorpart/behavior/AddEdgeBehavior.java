81e5ae9af3a870d867590b69892f5754e2665691
alexdp <alexdp@a3dc6853-a221-45bc-9d17-2c9bfbe3703c>
2012-11-12 16:21:06 +0000

    support to connect edge by two clicks and by drag and drop
    git-svn-id: https://violet.svn.sourceforge.net/svnroot/violet@260 a3dc6853-a221-45bc-9d17-2c9bfbe3703c

--- VioletFramework/src/main/java/com/horstmann/violet/workspace/editorpart/behavior/AddEdgeBehavior.java
+++ VioletFramework/src/main/java/com/horstmann/violet/workspace/editorpart/behavior/AddEdgeBehavior.java
@@ -31,65 +31,119 @@ public class AddEdgeBehavior extends AbstractEditorPartBehavior
     @Override
     public void onMousePressed(MouseEvent event)
     {
-        if (event.getClickCount() > 1)
+        if (!isConditionOK(event))
         {
+            cancel();
             return;
         }
-        if (event.getButton() != MouseEvent.BUTTON1) {
-            return;
-        }
-        if (GraphTool.SELECTION_TOOL.equals(this.graphToolsBar.getSelectedTool()))
+        if (!this.isLinkingInProgress)
         {
+            startAction(event);
             return;
         }
-        GraphTool selectedTool = this.selectionHandler.getSelectedTool();
-        if (!IEdge.class.isInstance(selectedTool.getNodeOrEdge()))
+        if (this.isLinkingInProgress && this.isLinkByTwoClicks)
         {
+            endAction(event);
             return;
         }
-        double zoom = editorPart.getZoomFactor();
-        final Point2D mousePoint = new Point2D.Double(event.getX() / zoom, event.getY() / zoom);
-        INode targetNode = graph.findNode(mousePoint);
-        this.isDraggingInProgress = (targetNode != null);
-        mouseDownPoint = mousePoint;
-        lastMousePoint = mousePoint;
     }
 
     @Override
     public void onMouseDragged(MouseEvent event)
     {
-        if (!isDraggingInProgress)
+        if (!this.isLinkingInProgress)
         {
             return;
         }
-        double zoom = editorPart.getZoomFactor();
-        Point2D mousePoint = new Point2D.Double(event.getX() / zoom, event.getY() / zoom);
-        lastMousePoint = mousePoint;
+        repaintOnMouseMoved(event);
+    }
+
+    @Override
+    public void onMouseMoved(MouseEvent event)
+    {
+        if (!this.isLinkingInProgress)
+        {
+            return;
+        }
+        this.isLinkByTwoClicks = true;
+        repaintOnMouseMoved(event);
     }
 
     @Override
     public void onMouseReleased(MouseEvent event)
     {
-        if (!isDraggingInProgress)
+        if (this.isLinkByTwoClicks)
         {
             return;
+        }
+        if (this.isLinkingInProgress)
+        {
+            endAction(event);
+            return;
+        }
+    }
+
+    private void repaintOnMouseMoved(MouseEvent event)
+    {
+        double zoom = this.editorPart.getZoomFactor();
+        Point2D mousePoint = new Point2D.Double(event.getX() / zoom, event.getY() / zoom);
+        lastMousePoint = mousePoint;
+        this.editorPart.getSwingComponent().doLayout();
+        this.editorPart.getSwingComponent().repaint();
+    }
+
+    private boolean isConditionOK(MouseEvent event)
+    {
+        if (event.getClickCount() > 1)
+        {
+            return false;
+        }
+        if (event.getButton() != MouseEvent.BUTTON1)
+        {
+            return false;
+        }
+        if (GraphTool.SELECTION_TOOL.equals(this.graphToolsBar.getSelectedTool()))
+        {
+            return false;
         }
         GraphTool selectedTool = this.selectionHandler.getSelectedTool();
         if (!IEdge.class.isInstance(selectedTool.getNodeOrEdge()))
         {
-            isDraggingInProgress = false;
-            return;
+            return false;
         }
+        return true;
+    }
+
+    private void startAction(MouseEvent event)
+    {
+        double zoom = editorPart.getZoomFactor();
+        final Point2D mousePoint = new Point2D.Double(event.getX() / zoom, event.getY() / zoom);
+        INode targetNode = graph.findNode(mousePoint);
+        this.isLinkingInProgress = (targetNode != null);
+        firstMousePoint = mousePoint;
+        lastMousePoint = mousePoint;
+    }
+
+    private void endAction(MouseEvent event)
+    {
+        GraphTool selectedTool = this.selectionHandler.getSelectedTool();
         IEdge prototype = (IEdge) selectedTool.getNodeOrEdge();
         IEdge newEdge = (IEdge) prototype.clone();
         newEdge.setId(new Id());
-        
-        boolean added = addEdgeAtPoints(newEdge, mouseDownPoint, lastMousePoint);
+
+        boolean added = addEdgeAtPoints(newEdge, firstMousePoint, lastMousePoint);
         if (added)
         {
             selectionHandler.setSelectedElement(newEdge);
-            isDraggingInProgress = false;
+            isLinkingInProgress = false;
+            isLinkByTwoClicks = false;
         }
+    }
+
+    private void cancel()
+    {
+        this.isLinkingInProgress = false;
+        this.isLinkByTwoClicks = false;
     }
 
     /**
@@ -112,19 +166,22 @@ public class AddEdgeBehavior extends AbstractEditorPartBehavior
                 INode endNode = graph.findNode(endPoint);
                 Point2D relativeStartPoint = null;
                 Point2D relativeEndPoint = null;
-                if (startNode != null) {
+                if (startNode != null)
+                {
                     Point2D startNodeLocationOnGraph = startNode.getLocationOnGraph();
                     double relativeStartX = startPoint.getX() - startNodeLocationOnGraph.getX();
                     double relativeStartY = startPoint.getY() - startNodeLocationOnGraph.getY();
                     relativeStartPoint = new Point2D.Double(relativeStartX, relativeStartY);
                 }
-                if (endNode != null) {
+                if (endNode != null)
+                {
                     Point2D endNodeLocationOnGraph = endNode.getLocationOnGraph();
                     double relativeEndX = endPoint.getX() - endNodeLocationOnGraph.getX();
                     double relativeEndY = endPoint.getY() - endNodeLocationOnGraph.getY();
                     relativeEndPoint = new Point2D.Double(relativeEndX, relativeEndY);
                 }
-                if (graph.connect(newEdge, startNode, relativeStartPoint, endNode, relativeEndPoint));
+                if (graph.connect(newEdge, startNode, relativeStartPoint, endNode, relativeEndPoint))
+                ;
                 {
                     newEdge.incrementRevision();
                     isAdded = true;
@@ -141,13 +198,13 @@ public class AddEdgeBehavior extends AbstractEditorPartBehavior
     @Override
     public void onPaint(Graphics2D g2)
     {
-        if (!isDraggingInProgress)
+        if (!isLinkingInProgress)
         {
             return;
         }
         Color oldColor = g2.getColor();
         g2.setColor(PURPLE);
-        g2.draw(new Line2D.Double(mouseDownPoint, lastMousePoint));
+        g2.draw(new Line2D.Double(firstMousePoint, lastMousePoint));
         g2.setColor(oldColor);
     }
 
@@ -155,7 +212,7 @@ public class AddEdgeBehavior extends AbstractEditorPartBehavior
 
     private static final int CONNECT_THRESHOLD = 8;
 
-    private Point2D mouseDownPoint = null;
+    private Point2D firstMousePoint = null;
 
     private Point2D lastMousePoint = null;
 
@@ -169,5 +226,8 @@ public class AddEdgeBehavior extends AbstractEditorPartBehavior
 
     private IGraphToolsBar graphToolsBar;
 
-    private boolean isDraggingInProgress = false;
+    private boolean isLinkingInProgress = false;
+
+    private boolean isLinkByTwoClicks = false;
+
 }
